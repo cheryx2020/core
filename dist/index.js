f3e6349eb25c4d0571942fbe3fd2bf8231dd5608
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2023-10-01 15:08:44 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import Link from 'next/link';
 
 var styles = "@-webkit-keyframes slideInLeft {\n  0% {\n    -webkit-transform: translateX(-100%);\n    transform: translateX(-100%);\n    visibility: visible;\n  }\n  100% {\n    -webkit-transform: translateX(0);\n    transform: translateX(0);\n  }\n}\n@keyframes slideInLeft {\n  0% {\n    -webkit-transform: translateX(-100%);\n    transform: translateX(-100%);\n    visibility: visible;\n  }\n  100% {\n    -webkit-transform: translateX(0);\n    transform: translateX(0);\n  }\n}\n.wrapper {\n  margin-top: 13px;\n  font-size: 18px;\n  line-height: 21px;\n  font-family: \"Big Shoulders Text\";\n  color: #676565;\n  display: flex;\n  align-self: flex-start;\n}\n.wrapper .spliter {\n  padding: 0px 12px;\n}\n@media (max-width: 641px) {\n  .wrapper .spliter {\n    padding: 0px 3px;\n  }\n}\n@media (max-width: 641px) {\n  .wrapper {\n    font-size: 16px;\n    width: 100%;\n    margin-top: 15px;\n    margin-bottom: 5px;\n  }\n  .wrapper a:first-child {\n    padding-left: 5px;\n  }\n}\n.wrapper .wrapperLink {\n  display: flex;\n}";
 
-// import Link from 'next/link';
 var SubLink = function SubLink(_ref) {
   var data = _ref.data,
     _ref$wrapperStyle = _ref.wrapperStyle,
@@ -16,7 +15,9 @@ var SubLink = function SubLink(_ref) {
     return /*#__PURE__*/React.createElement("div", {
       className: "".concat(styles.wrapperLink, " ").concat(className),
       key: i
-    }, i < data.length - 1 && /*#__PURE__*/React.createElement("div", {
+    }, /*#__PURE__*/React.createElement(Link, {
+      href: item.url
+    }, /*#__PURE__*/React.createElement("a", null, item.text)), i < data.length - 1 && /*#__PURE__*/React.createElement("div", {
       className: styles.spliter
     }, '>'));
   }));
--- src/components/sub-link/sub-link.js
+++ src/components/sub-link/sub-link.js
@@ -1,11 +1,10 @@
-// import Link from 'next/link';
-import React from 'react';
+import Link from 'next/link';
 import styles from './Sublink.module.scss';
 const SubLink = ({data, wrapperStyle = {}, className=''}) => {
     return <div className={styles.wrapper} style={wrapperStyle}>
         {Array.isArray(data) && data.length > 0 && data.map((item,i) => {
             return <div className={`${styles.wrapperLink} ${className}`} key={i}>
-            {/* <Link href={item.url}><a>{item.text}</a></Link> */}
+            <Link href={item.url}><a>{item.text}</a></Link>
             {i < data.length - 1 && <div className={styles.spliter}>{'>'}</div>}
             </div>
         })}
