7e599a4d48672bdcee268f43eaed5bccedb5fb1f
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2023-10-01 15:25:43 +0700

    update

--- src/components/sub-link/sub-link.js
+++ src/components/sub-link/sub-link.js
@@ -1,4 +1,6 @@
 import styles from './Sublink.module.scss';
+import React from 'react';
+
 const SubLink = ({data, wrapperStyle = {}, className='', renderItem = () => {}}) => {
     return <div className={styles.wrapper} style={wrapperStyle}>
         {Array.isArray(data) && data.length > 0 && data.map((item,i) => {
