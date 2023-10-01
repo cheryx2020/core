14ae0041a476dbfad78e94a56fa74ad052df1f46
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2023-10-01 14:40:05 +0700

    Text

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from "@rollup/plugin-commonjs";
 import sass from 'rollup-plugin-sass';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import resolve  from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 
@@ -9,16 +9,16 @@ export default {
   input: 'index.js',
   plugins: [
     peerDepsExternal(),
-    nodeResolve({
+    resolve({
       browser: true
     }),
+    commonjs({ include: '/node_modules/' }),
     babel({
       babelrc: true,
       babelHelpers: "runtime",
       exclude: 'node_modules/**', // only transpile our source code
       extensions: [".js"],
     }),
-    commonjs(),
     sass(),
   ],
   output: {
