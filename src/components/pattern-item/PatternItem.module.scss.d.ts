a13c1b935024f6c48d525e46b28a04819e41a485
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2024-01-21 21:32:58 +0700

    update

---
+++ src/components/pattern-item/PatternItem.module.scss.d.ts
@@ -0,0 +1,28 @@
+export const wrapper: string
+export const isBottom: string
+export const freeTag: string
+export const editMenu: string
+export const isFree: string
+export const button: string
+export const img: string
+export const description: string
+export const name: string
+export const mobileContent: string
+export const patternUpload: string
+export const label: string
+interface Namespace {
+	"wrapper": string,
+	"isBottom": string,
+	"freeTag": string,
+	"editMenu": string,
+	"isFree": string,
+	"button": string,
+	"img": string,
+	"description": string,
+	"name": string,
+	"mobileContent": string,
+	"patternUpload": string,
+	"label": string,
+}
+declare const patternItemModuleScss: Namespace
+export default patternItemModuleScss
---
+++ src/components/pattern-name/PatternName.module.scss.d.ts
@@ -0,0 +1,12 @@
+export const wrapper: string
+export const isBottom: string
+export const menu: string
+export const pickerMenu: string
+interface Namespace {
+	"wrapper": string,
+	"isBottom": string,
+	"menu": string,
+	"pickerMenu": string,
+}
+declare const patternNameModuleScss: Namespace
+export default patternNameModuleScss
