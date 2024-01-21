658b2427efe721671d855a3eb73544b0958ee1da
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2024-01-21 21:30:11 +0700

    fix core

---
+++ src/components/pattern-name/pattern-name.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
+import styles from './PatternName.module.scss';
+const PatternName = ({ onBlur = () => { }, nameColor, isEdit, text, onChangeColor = () => {}, isBottom }) => {
+  const [colorInstance, setColorInstance] = useState({
+    r: '241',
+    g: '112',
+    b: '19',
+    a: '1',
+  });
+  const [showPicker, setShowPicker] = useState(false);
+  return <div className={`${styles.wrapper} ${isBottom ? styles.isBottom : ''}`} style={{ color: nameColor }}><div suppressContentEditableWarning={true} contentEditable={isEdit ? 'true' : 'false'} onBlur={onBlur}>{text}</div>
+    {isEdit && <div onClick={() => setShowPicker(true)} suppressContentEditableWarning={true} contentEditable='false' className={styles.menu}>
+      {showPicker && <div className={styles.pickerMenu} onMouseLeave={() => setShowPicker(false)}>
+        <SketchPicker color={colorInstance} onChangeComplete={color => { onChangeColor(`rgb(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`); setColorInstance(color.rgb)}} />
+      </div>
+      }
+    </div>}
+  </div>
+}
+export default PatternName;
