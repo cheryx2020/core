bb50c15edd875d8f37b39d6cd484f8b4cc1ad863
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2025-01-04 22:56:48 +0700

    publish update title link

--- src/components/title-link/title-link.js
+++ src/components/title-link/title-link.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './TitleLink.module.scss';
 
-const TitleLink = ({text, url, className, Link}) => {
-  return <div className={`${styles.wrapper} ${className}`}>
+const TitleLink = ({text, linkText = 'Xem thêm >>', url, className, Link, styles: wrapperStyle = {}}) => {
+  return <div style={wrapperStyle} className={`${styles.wrapper} ${className}`}>
     <div className={styles.text}>{text}</div>
-    {url && <Link href={url}><a>{'Xem thêm >>'}</a></Link>}
+    {url && <Link href={url}><a>{linkText}</a></Link>}
   </div>
 }
 export default TitleLink;
