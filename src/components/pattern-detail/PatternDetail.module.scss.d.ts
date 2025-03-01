5fb55c13a2fdd4abbdea50bb9379ffc6e0249910
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2025-03-01 10:14:59 +0700

    publish add style discount

--- src/components/pattern-detail/PatternDetail.module.scss.d.ts
+++ src/components/pattern-detail/PatternDetail.module.scss.d.ts
@@ -3,6 +3,10 @@ export const mainImage: string
 export const rightInfo: string
 export const title: string
 export const author: string
+export const price: string
+export const lineThrough: string
+export const discounted: string
+export const priceNote: string
 export const storeInfo: string
 export const blackCatWrapper: string
 export const blackCat: string
@@ -11,12 +15,12 @@ export const text: string
 export const supperWrapperButton: string
 export const buttonWrapper: string
 export const priceWrapper: string
-export const price: string
 export const payPalWrapper: string
 export const show: string
 export const payPal: string
 export const closeLink: string
 export const linkStore: string
+export const discount: string
 export const mb11: string
 export const paypalButton: string
 export const listSmallImage: string
@@ -29,6 +33,10 @@ interface Namespace {
 	"rightInfo": string,
 	"title": string,
 	"author": string,
+	"price": string,
+	"lineThrough": string,
+	"discounted": string,
+	"priceNote": string,
 	"storeInfo": string,
 	"blackCatWrapper": string,
 	"blackCat": string,
@@ -37,12 +45,12 @@ interface Namespace {
 	"supperWrapperButton": string,
 	"buttonWrapper": string,
 	"priceWrapper": string,
-	"price": string,
 	"payPalWrapper": string,
 	"show": string,
 	"payPal": string,
 	"closeLink": string,
 	"linkStore": string,
+	"discount": string,
 	"mb11": string,
 	"paypalButton": string,
 	"listSmallImage": string,
