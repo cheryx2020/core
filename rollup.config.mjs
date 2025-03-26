b7d1efd6ba6f6a35a9a167f86b5c93e3a97ebedd
glorynguyen <nguyenbavinhcntt@gmail.com>
2025-03-26 16:43:43 +0700

    publish update rollup plugin terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,32 @@
-import { babel } from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from '@rollup/plugin-node-resolve';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss-modules'
-import autoprefixer from 'autoprefixer'
-import copy from 'rollup-plugin-copy'
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss-modules";
+import autoprefixer from "autoprefixer";
+import copy from "rollup-plugin-copy";
 
 export default {
-  input: 'index.js',
+  input: "index.js",
   plugins: [
     peerDepsExternal(),
     resolve({
-      browser: true
+      browser: true,
     }),
     commonjs({
-      include: "node_modules/**"
+      include: "node_modules/**",
     }),
     babel({
       babelrc: true,
       babelHelpers: "runtime",
-      exclude: 'node_modules/**', // only transpile our source code
+      exclude: "node_modules/**", // only transpile our source code
       presets: ["@babel/preset-react"],
       extensions: [".js"],
     }),
     terser(),
     postcss({
-      extract: true,  // extracts to `${basename(dest)}.css`
+      extract: true, // extracts to `${basename(dest)}.css`
       plugins: [autoprefixer()],
       writeDefinitions: true,
       minimize: true,
@@ -34,13 +34,11 @@ export default {
     }),
     copy({
       // Specify the files or folders to copy
-      targets: [
-        { src: 'src/components/styles/*.scss', dest: 'dist/styles' },
-      ]
-    })
+      targets: [{ src: "src/components/styles/*.scss", dest: "dist/styles" }],
+    }),
   ],
   output: {
-    dir: 'dist',
-    format: 'es'
+    dir: "dist",
+    format: "es",
   },
 };
